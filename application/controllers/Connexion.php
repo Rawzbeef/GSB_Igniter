785c3dee0090216997f412f32574c05395be9da7
Rawzbeef <vincent.gaspar@hotmail.fr>
2019-03-12 13:40:18 +0100

    12/03/2019

--- application/controllers/Connexion.php
+++ application/controllers/Connexion.php
@@ -6,5 +6,5 @@ class Connexion extends CI_Controller {
         $this->load->view('v_head', $data);
         $this->load->view('v_menu', $data); //pour faciliter l'accès en attendant que ce soit codé, a supprimer quand terminé
         $this->load->view('v_connexion');
-	}
+    }
 }
